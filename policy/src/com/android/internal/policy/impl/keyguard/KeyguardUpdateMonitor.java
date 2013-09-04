3cf56f4ef05156b88fa3ac54af0eb28291522a4f
fitsnugly <rickfitsnugly@gmail.com>
2013-09-04 09:11:16 -0400

    lockscreen battery status: add off option (2/2)
    Change-Id: Ie10015865f161e783dc394d467e456d51dc9fbbc
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	policy/src/com/android/internal/policy/impl/keyguard/KeyguardUpdateMonitor.java

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardUpdateMonitor.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardUpdateMonitor.java
@@ -617,7 +617,8 @@ public class KeyguardUpdateMonitor {
         return mKeyguardIsVisible;
     }
 
-    private static boolean isBatteryUpdateInteresting(BatteryStatus old, BatteryStatus current, Context context) {
+    private static boolean isBatteryUpdateInteresting(BatteryStatus old,
+            BatteryStatus current, Context context) {
         final boolean nowPluggedIn = current.isPluggedIn();
         final boolean wasPluggedIn = old.isPluggedIn();
         final boolean stateChangedWhilePluggedIn =
@@ -635,7 +636,8 @@ public class KeyguardUpdateMonitor {
         }
 
         // change in battery level while plugged in or always interested
-        if ((nowPluggedIn || shouldAlwaysShowBatteryInfo(context) || current.isBatteryLow()) && old.level != current.level) {
+        if ((nowPluggedIn || shouldAlwaysShowBatteryInfo(context) || current.isBatteryLow())
+                && old.level != current.level) {
             return true;
         }
 
@@ -644,7 +646,12 @@ public class KeyguardUpdateMonitor {
 
     public static boolean shouldAlwaysShowBatteryInfo(Context context) {
         return Settings.System.getInt(context.getContentResolver(),
-                       Settings.System.LOCKSCREEN_ALWAYS_SHOW_BATTERY, 0) == 1;
+                Settings.System.LOCKSCREEN_BATTERY_VISIBILITY, 0) == 1;
+    }
+
+    public static boolean shouldNeverShowBatteryInfo(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.LOCKSCREEN_BATTERY_VISIBILITY, 0) == 2;
     }
 
     /**
