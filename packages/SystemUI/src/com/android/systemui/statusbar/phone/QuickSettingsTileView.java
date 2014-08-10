58a9a1af15f0efe91549c15f98e92ca6b53c288d
Gottardo Stefano <whiteflash@email.it>
2014-08-10 18:53:10 +0200

    Added some quick settings functions taked from commit: Integrate remote display providers into Quick Settings. https://github.com/CyanogenMod/android_frameworks_base/commit/635e91529681587a65ca05cf27d5de78b368ead7
    Change-Id: I4e38d7e701d801b662ba02eefb646b898a437565

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsTileView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsTileView.java
@@ -20,6 +20,7 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewParent;
 import android.widget.FrameLayout;
 
 /**
@@ -28,13 +29,13 @@ import android.widget.FrameLayout;
 public class QuickSettingsTileView extends FrameLayout {
 
     private int mColSpan;
-    private final int mRowSpan;
+    private boolean mPrepared;
+    private OnPrepareListener mOnPrepareListener;
 
     public QuickSettingsTileView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         mColSpan = 1;
-        mRowSpan = 1;
     }
 
     void setColumnSpan(int span) {
@@ -62,5 +63,74 @@ public class QuickSettingsTileView extends FrameLayout {
             }
         }
         super.setVisibility(vis);
+    }
+
+    public void setOnPrepareListener(OnPrepareListener listener) {
+        if (mOnPrepareListener != listener) {
+            mOnPrepareListener = listener;
+            mPrepared = false;
+            post(new Runnable() {
+                @Override
+                public void run() {
+                    updatePreparedState();
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onVisibilityChanged(View changedView, int visibility) {
+        super.onVisibilityChanged(changedView, visibility);
+        updatePreparedState();
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        updatePreparedState();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        updatePreparedState();
+    }
+
+    private void updatePreparedState() {
+        if (mOnPrepareListener != null) {
+            if (isParentVisible()) {
+                if (!mPrepared) {
+                    mPrepared = true;
+                    mOnPrepareListener.onPrepare();
+                }
+            } else if (mPrepared) {
+                mPrepared = false;
+                mOnPrepareListener.onUnprepare();
+            }
+        }
+    }
+
+    private boolean isParentVisible() {
+        /* -- removed -- api level 19
+         * if (!isAttachedToWindow()) {
+            return false;
+        }*/
+        for (ViewParent current = getParent(); current instanceof View;
+                current = current.getParent()) {
+            View view = (View)current;
+            if (view.getVisibility() != VISIBLE) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Called when the view's parent becomes visible or invisible to provide
+     * an opportunity for the client to provide new content.
+     */
+    public interface OnPrepareListener {
+        void onPrepare();
+        void onUnprepare();
     }
 }
